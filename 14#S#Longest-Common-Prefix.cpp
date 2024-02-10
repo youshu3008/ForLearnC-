96a1ad766d5b279b1fcfa2140187456712e17bfa
youshu <log3008@163.com>
2024-02-10 23:57:32 +0800

    14

--- 14#S#Longest-Common-Prefix.cpp
+++ 14#S#Longest-Common-Prefix.cpp
@@ -5,37 +5,40 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (!strs.size()) {
-            return "";
-        }
-        string prefix = strs[0];
-        int count = strs.size();
-        for (int i = 1; i < count; ++i) {
-            prefix = longestCommonPrefix(prefix, strs[i]);
-            if (!prefix.size()) {
+      if(!strs.size()){
+        return "";
+      };
+      int len = strs.size();
+      string Prefix = strs[0];
+      for (int i = 1; i < len; ++i)
+      {
+        Prefix = longestCommonPrefix(Prefix,strs[i]);
+        if (!Prefix.size()) {
                 break;
-            }
+            };
+      };
+      return Prefix;
+    };
+    string longestCommonPrefix(string &str1,string &str2){
+    int lens = min(str1.size(),str2.size());
+    string Prefixs = "";
+    for(int i=0;i<lens;++i){
+        if(str1[i]==str2[i]){
+            Prefixs = str1.substr(0,i+1);
         }
-        return prefix;
-    }
-
-    string longestCommonPrefix(const string& str1, const string& str2) {
-        int length = min(str1.size(), str2.size());
-        int index = 0;
-        while (index < length && str1[index] == str2[index]) {
-            ++index;
+        else{
+            break;
         }
-        return str1.substr(0, index);
-    }
-}
-
-
+    };
+    return Prefixs;
+    };
+};
 
 int main() {
     Solution solution;
 
     // 创建字符串向量
-    vector<string> input1 = {"flower", "flow", "flight"};
+    vector<string> input1 = {"cir","car"};
     vector<string> input2 = {"flower", "aflower", "afyflow"};
 
     // 测试示例
