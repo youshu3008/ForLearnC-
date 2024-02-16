ee39a501947d6d4f1d832633b26677fcedf70df6
youshu <log3008@163.com>
2024-02-16 23:32:14 +0800

    11

---
+++ 103#M#binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        if (root) q.push(root);
+        bool rev = false;
+        while (q.size()) {
+            int sz = q.size();
+            vector<int> tmp;
+            while (sz--) {
+                auto cur = q.front(); q.pop();
+                tmp.push_back(cur->val);
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
+            }
+            if (rev) ranges::reverse(tmp);
+            ans.push_back(tmp);
+            rev ^= 1;
+        }
+        return ans;
+    }
+};
+
+
+int main() {
+     Solution solution;
+
+    // 创建字符串向量
+    vector<int> input1 = {3,9,20,null,null,15,7};
+    vector<int> input2 = {1};
+    vector<int> input3 = {};
+
+    // 测试示例
+    vector<vector<int>> output1 = solution.zigzagLevelOrder(input1);
+    vector<vector<int>> output2 = solution.zigzagLevelOrder(input2);
+    vector<vector<int>> output3 = solution.zigzagLevelOrder(input3);
+
+    // 输出结果
+    cout << "[";
+    for (const auto& group : output1) {
+        cout << "[";
+        for (const auto& str : group) {
+            cout << "\"" << str << "\",";
+        }
+        cout << "],";
+    }
+    cout << "]" << endl;
+
+    cout << "[";
+    for (const auto& group : output2) {
+        cout << "[";
+        for (const auto& str : group) {
+            cout << "\"" << str << "\",";
+        }
+        cout << "],";
+    }
+    cout << "]" << endl;
+
+    cout << "[";
+    for (const auto& group : output3) {
+        cout << "[";
+        for (const auto& str : group) {
+            cout << "\"" << str << "\",";
+        }
+        cout << "],";
+    }
+    cout << "]" << endl;
+    system("pause");
+    return 0;
+}
+
+
+/*
+https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/description/
+给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。
+（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+
+示例 1：
+
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[20,9],[15,7]]
+示例 2：
+
+输入：root = [1]
+输出：[[1]]
+示例 3：
+
+输入：root = []
+输出：[]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 2000] 内
+-100 <= Node.val <= 100
+*/
--- 11#M#container-with-most-water.cpp
+++ 11#M#container-with-most-water.cpp
@@ -5,46 +5,35 @@
 #include <unordered_set>
 
 using namespace std;
+// 时间复杂度O(n^2)
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int ans = 0;
+//         for (int left = 0; left < height.size(); left++)
+//         {
+//            for (int right = left; right < height.size(); right++)
+//            {
+//             int chang = right - left ;
+//             ans = max(ans,min(height[left]*chang,height[right]*chang));
+//            }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-
+        int i = 0, j = height.size() - 1, res = 0;
+        while(i < j) {
+            res = height[i] < height[j] ? 
+                max(res, (j - i) * height[i++]): 
+                max(res, (j - i) * height[j--]); 
+        }
+        return res;  
     }
 };
-
-// class Solution {
-// public:
-//     void moveZeroes(vector<int>& nums) {
-//         int nonZeroIndex = 0; // 用于记录非零元素的位置
-//         // 将非零元素移到数组前部
-//         for (int i = 0; i < nums.size(); ++i) {
-//             if (nums[i] != 0) {
-//                 nums[nonZeroIndex++] = nums[i];
-//             }
-//         }
-//         // 将剩余位置补零
-//         while (nonZeroIndex < nums.size()) {
-//             nums[nonZeroIndex++] = 0;
-//         }      
-//     }
-// };
-
-// class Solution {
-// public:
-//     void moveZeroes(vector<int>& nums) {
-//         int dest = -1,cur = 0;
-//         while(cur<nums.size())
-//         {
-//             if(nums[cur]!=0)
-//             {
-//                 swap(nums[dest+1],nums[cur]);
-//                 ++dest;++cur;
-//             }
-//             else
-//             ++cur;
-//         }
-//     }
-// };
 
 
 int main() {
@@ -55,19 +44,10 @@ int main() {
     vector<int> input2 = {1,1};
   
     // 测试示例
-    solution.maxArea(input1);
-    solution.maxArea(input2);
 
     // 输出结果
-    for (int num : input1) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    for (int num : input2) {
-        cout << num << " ";
-    }
-    cout << endl;
+    cout << solution.maxArea(input1) << endl;
+    cout << solution.maxArea(input2) << endl;
 
     system("pause");
     return 0;
