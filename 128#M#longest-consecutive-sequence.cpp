b7f2868a4af46f25dac72e7451e7dc74da847537
youshu <log3008@163.com>
2024-02-16 17:56:48 +0800

    128

--- 128#M#longest-consecutive-sequence.cpp
+++ 128#M#longest-consecutive-sequence.cpp
@@ -2,34 +2,73 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <unordered_set>
 
 using namespace std;
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& nums) {
-//         sort(nums.begin(),nums.end());
-//         for(auto it = nums.begin();it != nums.end(); it++){
-//             if(it->second + 1 != (++it)->second ) 
-//         }
-        
-//     }
-// };
 
+// 时间复杂度O(n)
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int res = 1,len = 1;
-        if(nums.size() == 0) return 0;
-        for(int i = 1;i < nums.size();i++){
-            if(nums[i]==nums[i-1])continue;//去重
-           if(nums[i] == nums[i-1] + 1)len++;
-           else len = 1;
-           res = max(res,len);
+        unordered_set<int> numSet(nums.begin(), nums.end());  // 将 nums 转换为哈希表，提高查找速度
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            // 只考虑连续序列的起始元素，因为后续元素会在其他起始元素的迭代中处理
+            if (numSet.find(num - 1) == numSet.end()) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                // 继续查找连续序列的下一个元素
+                while (numSet.find(currentNum + 1) != numSet.end()) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = max(longestStreak, currentStreak);
+            }
         }
-        return res;
+
+        return longestStreak;
     }
 };
+
+// 时间复杂度O(nlogn)
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         if(nums.size() == 0) return 0;
+           // 这一步最大时间复杂度O(nlogn)
+//         sort(nums.begin(),nums.end()); 
+//         int res = 1,len = 1;
+//         for(int i = 1;i < nums.size();i++){
+//             if(nums[i]==nums[i-1])continue;//去重
+//            if(nums[i] == nums[i-1] + 1)len++;
+//            else len = 1;
+//            res = max(res,len);
+//         }
+//         return res;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//        sort(nums.begin(),nums.end());
+//        int res = 1 ,lianxu = 1;
+//        for(int i= 0; i < nums.size(); i++){
+//             if(nums[i-1] == nums[i]) continue;
+//             if(nums[i-1] == nums[i]-1){
+//                 lianxu++;
+//             }
+//             else{
+//                 lianxu = 1;
+//             }
+//             res = max(res,lianxu);
+//        }
+//        return res;
+//     }
+// };
 
 int main() {
      Solution solution;
